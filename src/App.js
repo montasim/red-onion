46070a4a9f3725d4ce98fbeef53c3b6ac61bad18
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-12 18:19:07 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Pages/Home/Home/Home';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
     <div>
-
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
