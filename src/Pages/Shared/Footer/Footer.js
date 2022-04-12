7ccc6667d30a31e2043cb46e28da216133731659
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-12 18:44:43 +0600

    footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FiFacebook } from 'react-icons/fi';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 const Footer = () => {
     const today = new Date();
     const year = today.getFullYear();
     return (
-        <div className='mt-5 bg-dark text-center text-white'>
-            <p className='py-5'>Red Onion &copy; {year}</p>
+        <div className='mt-5 bg-dark text-white items-center'>
+            <p className='py-5 text-center'>Red Onion &copy; {year} <span className='items-right'><FiFacebook /><AiFillGithub /> <AiFillLinkedin /></span></p>
         </div>
     );
 };
