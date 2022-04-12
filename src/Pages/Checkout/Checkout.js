dcc3f814098afac04d53779c1e42bb0099d61d52
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-12 19:38:39 +0600

    checkout form

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
 
 const Checkout = () => {
     return (
-        <div>
-            <h2>Checkout</h2>
+        <div className='my-5 w-lg-50'>
+            <Container>
+                <h2 className='mb-4 text-primary'>Please Fill The Form To Continue</h2>
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" readOnly />
+                        <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text" placeholder="Your Name" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicAddress">
+                        <Form.Label>Address</Form.Label>
+                        <Form.Control type="text" placeholder="Your Address" required />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPhone">
+                        <Form.Label>Phone</Form.Label>
+                        <Form.Control type="phone" placeholder="Your Phone Number" required />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </Container>
         </div>
     );
 };
