79ebbfd49b842439de6d38cae3f2e75bdad03eb4
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-12 22:10:54 +0600

    signup & login system

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { AiFillGoogleCircle, AiOutlineGithub } from 'react-icons/ai';
 import { FaFacebookF } from 'react-icons/fa';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../Hooks/Firebase.Init';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navaigate = useNavigate();
+    const location = useLocation();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    const from = location?.state?.from?.pathname || '/checkout';
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    if (user) {
+        console.log(user);
+        navaigate(from, { replace: true });
+    }
+
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password);
+    }
+
     return (
         <div className='my-5 w-lg-50'>
             <Container>
                 <h2 className='mb-4 text-primary'>Please Login Here</h2>
-                <Form>
+                <Form onSubmit={handleUserSignIn}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -19,8 +53,12 @@ const Login = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required />
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
                     </Form.Group>
+                    <p style={{ color: 'red' }}>{error?.message}</p>
+                    {
+                        loading && <p>... loading</p>
+                    }
                     <p>New to Red Onion <span className='text-primary'><a href="/signup">Please Signup Here</a></span></p>
                     <Button variant="primary" type="submit">
                         Submit
--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { AiFillGoogleCircle, AiOutlineGithub } from 'react-icons/ai';
 import { FaFacebookF } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../Hooks/Firebase.Init';
 
 const Signup = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+
+    const handleEmailBlur = event => {
+        setEmail(event.target.value);
+    }
+
+    const handlePasswordBlur = event => {
+        setPassword(event.target.value);
+    }
+
+    const handleConfirmPasswordBlur = event => {
+        setConfirmPassword(event.target.value);
+    }
+
+    if (user) {
+        navigate('/login')
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError('Password Not Matched');
+            return;
+        }
+        if (password.length < 6) {
+            setError('Password Must Be 6 Characters Long');
+            return;
+        }
+
+        createUserWithEmailAndPassword(email, password)
+            .then(result => {
+                console.log('user created');
+            })
+    }
+
     return (
         <div className='my-5 w-lg-50'>
             <Container>
                 <h2 className='mb-4 text-primary'>Please Signup Here</h2>
-                <Form>
+                <Form onSubmit={handleCreateUser}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" required />
                         <Form.Text className="text-muted">
                             We'll never share your email with anyone else.
                         </Form.Text>
@@ -19,11 +64,11 @@ const Signup = () => {
 
                     <Form.Group className="mb-3" controlId="formBasicPassword">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" required />
+                        <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
                         <Form.Label>Confirm Password</Form.Label>
-                        <Form.Control type="password" placeholder="Confirm Password" required />
+                        <Form.Control onBlur={handleConfirmPasswordBlur} type="password" placeholder="Confirm Password" required />
                     </Form.Group>
                     <p>Already A Member <span className='text-primary'><a href="/login">Login Here</a></span></p>
                     <Button variant="primary" type="submit">
