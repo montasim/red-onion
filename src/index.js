5c34e2bc2686e1b22c3a15a0dfabf8560d480820
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-04-12 23:02:02 +0600

    upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>,
   </BrowserRouter>,
-  document.getElementById('root')
 );
